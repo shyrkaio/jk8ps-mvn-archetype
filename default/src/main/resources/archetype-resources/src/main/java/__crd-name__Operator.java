398a17bcdb70c49c026f58829e6ce7486ca15b93
Charles Sabourdin <kanedafromparis@gmail.com>
2021-10-20 20:49:38 +0200

    fix:operator start

--- default/src/main/resources/archetype-resources/src/main/java/__crd-name__Operator.java
+++ default/src/main/resources/archetype-resources/src/main/java/__crd-name__Operator.java
@@ -15,6 +15,9 @@ import java.io.IOException;
 
 public class ${crd-name}Operator {
 
+    private static final Logger log = LoggerFactory.getLogger(${crd-name}Operator.class);
+
+
     public static void main(String[] args) throws IOException {
 
         Config config = new ConfigBuilder().withNamespace(null).build();
@@ -23,6 +26,7 @@ public class ${crd-name}Operator {
 
         ${crd-name}Controller ${crd-name.toLowerCase()}Controller = new ${crd-name}Controller(client);
         operator.register(${crd-name.toLowerCase()}Controller);
+        operator.start();
 
         new FtBasic(new TkFork(new FkRegex("/health", "ALL GOOD.")), 8080).start(Exit.NEVER);
     }
